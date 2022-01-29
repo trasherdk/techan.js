7c6c851bae9d643b4d49ab75209b20195aa396d1
TrasherDK <trasherdk@fumlersoft.dk>
2022-01-29 05:02:24 +0100

    library: standard refactoring

--- examples/lib/library.js
+++ examples/lib/library.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const lastdata = {
 	time: 0,
 	volume: 0,
@@ -7,13 +5,13 @@ const lastdata = {
 	high: 0,
 	low: 0,
 	close: 0
-};
+}
 
 const defparam = {
-	dataurl: "https://min-api.cryptocompare.com/data/",
+	dataurl: 'https://min-api.cryptocompare.com/data/',
 	symbol: {
-		EUR: "€",
-		USD: "$",
+		EUR: '€',
+		USD: '$'
 	},
 	exchange: 'Kraken',
 	crypto: 'XMR',
@@ -31,75 +29,74 @@ const defparam = {
 		url: 'histoday',
 		limit: 180 // (6 * 30)
 	}
-};
+}
 
-const margin = { top: 20, right: 50, bottom: 30, left: 80 };
-const width = 600 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
-
+const margin = { top: 20, right: 50, bottom: 30, left: 80 }
+const width = 600 - margin.left - margin.right
+const height = 400 - margin.top - margin.bottom
 
 const _ = (id) => document.getElementById(id)
 
 function getResolution (prmstr) {
 	switch (prmstr.res) {
 		case 'minute':
-			return defparam.minute;
+			return defparam.minute
 		case 'hour':
-			return defparam.hour;
+			return defparam.hour
 		case 'day':
-			return defparam.day;
+			return defparam.day
 		default:
-			return defparam.minute;
+			return defparam.minute
 	}
 }
 
 const timeAgo = (timestamp, locale = 'en') => {
-	let value;
-	const diff = (new Date().getTime() - timestamp.getTime()) / 1000;
-	const minutes = Math.floor(diff / 60);
-	const hours = Math.floor(minutes / 60);
-	const days = Math.floor(hours / 24);
-	const months = Math.floor(days / 30);
-	const years = Math.floor(months / 12);
-	const rtf = new Intl.RelativeTimeFormat(locale, { numeric: "auto" });
+	let value
+	const diff = (new Date().getTime() - timestamp.getTime()) / 1000
+	const minutes = Math.floor(diff / 60)
+	const hours = Math.floor(minutes / 60)
+	const days = Math.floor(hours / 24)
+	const months = Math.floor(days / 30)
+	const years = Math.floor(months / 12)
+	const rtf = new Intl.RelativeTimeFormat(locale, { numeric: 'auto' })
 
 	if (years > 0) {
-		value = rtf.format(0 - years, "year");
+		value = rtf.format(0 - years, 'year')
 	} else if (months > 0) {
-		value = rtf.format(0 - months, "month");
+		value = rtf.format(0 - months, 'month')
 	} else if (days > 0) {
-		value = rtf.format(0 - days, "day");
+		value = rtf.format(0 - days, 'day')
 	} else if (hours > 0) {
-		value = rtf.format(0 - hours, "hour");
+		value = rtf.format(0 - hours, 'hour')
 	} else if (minutes > 0) {
-		value = rtf.format(0 - minutes, "minute");
+		value = rtf.format(0 - minutes, 'minute')
 	} else {
-		value = rtf.format(0 - diff, "second");
+		value = rtf.format(0 - diff, 'second')
 	}
-	return value;
+	return value
 }
 
 function checkParams (paramobj) {
-	const { url, limit } = getResolution(paramobj);
+	const { url, limit } = getResolution(paramobj)
 
-	paramobj.resurl = url;
-	paramobj.agg = (paramobj.agg ? paramobj.agg : defparam.aggregate);
-	paramobj.e = (paramobj.e ? paramobj.e : defparam.exchange);
-	paramobj.currency = (paramobj.currency ? paramobj.currency : defparam.currency);
-	paramobj.crypto = (paramobj.crypto ? paramobj.crypto : defparam.crypto);
-	paramobj.limit = (paramobj.limit ? paramobj.limit : limit);
+	paramobj.resurl = url
+	paramobj.agg = (paramobj.agg ? paramobj.agg : defparam.aggregate)
+	paramobj.e = (paramobj.e ? paramobj.e : defparam.exchange)
+	paramobj.currency = (paramobj.currency ? paramobj.currency : defparam.currency)
+	paramobj.crypto = (paramobj.crypto ? paramobj.crypto : defparam.crypto)
+	paramobj.limit = (paramobj.limit ? paramobj.limit : limit)
 
-	return paramobj;
+	return paramobj
 }
 
 function getURL (paramobj) {
-	let url = defparam.dataurl + paramobj.resurl;
-	//	url += '?e=' + paramobj.e;
-	url += `?fsym=${paramobj.currency}`;
-	url += `&tsym=${paramobj.crypto}`;
-	url += `&limit=${paramobj.limit}`;
-	url += `&aggregate=${paramobj.agg}`;
-	return url;
+	let url = defparam.dataurl + paramobj.resurl
+
+	url += `?fsym=${paramobj.currency}`
+	url += `&tsym=${paramobj.crypto}`
+	url += `&limit=${paramobj.limit}`
+	url += `&aggregate=${paramobj.agg}`
+	return url
 }
 
 function getSearchParameters () {
@@ -109,17 +106,17 @@ function getSearchParameters () {
 	for (const key of searchParams.keys()) {
 		paramObj[key] = searchParams.getAll(key).length > 1
 			? searchParams.getAll(key)
-			: searchParams.get(key);
+			: searchParams.get(key)
 	}
 	return paramObj
 }
 
 function getReadableHashRateString (hashrate) {
-	let i = 0;
-	const byteUnits = [' H', ' KH', ' MH', ' GH', ' TH', ' PH'];
+	let i = 0
+	const byteUnits = [' H', ' KH', ' MH', ' GH', ' TH', ' PH']
 	while (hashrate > 1000) {
-		hashrate /= 1000;
-		i++;
+		hashrate /= 1000
+		i++
 	}
-	return hashrate.toFixed(2) + byteUnits[i];
+	return hashrate.toFixed(2) + byteUnits[i]
 }
